776750f9494900fd9d10078c57b08a5ba3b9a678
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-09-08 00:47:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,48 +30,48 @@ const sql = require("sqlite");
 
 
 
-const client = new Discord.Client({ fetchAllMembers: true });
-
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
 }
 client.on("ready", () => {
     var guild;
     while (!guild)
-        guild = client.guilds.find("name", "Mysterious..")
+        guild = client.guilds.get("432942113697562636");
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
             dat[Inv] = Invite.uses;
-        })
-    })
-})
+        });
+    });
+});
+
+
+
 client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'mysterious');
+    let channel = member.guild.channels.get("480734524796305428");
     if (!channel) {
-        console.log("!channel fails");
+        console.log("!the channel id it's not correct");
         return;
     }
     if (member.id == client.user.id) {
         return;
     }
-    console.log('made it till here!');
+    console.log('-');
     var guild;
     while (!guild)
-        guild = client.guilds.find("name", "Mysterious..")
+        guild = client.guilds.get("432942113697562636");
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
             if (dat[Inv])
                 if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(`**Invited By: ${Invite.inviter}**`)            
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
  }
             dat[Inv] = Invite.uses;
-        })
-    })
+       
+       });
+    });
 });
 
 
