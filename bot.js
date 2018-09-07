ae22931af8bfca05aaeadfa98c5f98518c0cc772
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-09-07 23:09:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 client.on('guildMemberAdd',async member => {
   const Canvas = require('canvas');
   const jimp = require('jimp');
-  const w = ['./welcome.png'];
+  const w = ['./welcome1.png'];
         let Image = Canvas.Image,
             canvas = new Canvas(800, 300),
             ctx = canvas.getContext('2d');
